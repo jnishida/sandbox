822123f1f48759bf9c7b81c7bb615ce50f1494ed
jnishida <nishida@allora-inc.jp>
2017-03-05 12:28:13 +0900

    WIP getSchedules

--- schedule/src/main/java/schedule/ScheduleManager.java
+++ schedule/src/main/java/schedule/ScheduleManager.java
@@ -5,9 +5,12 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class ScheduleManager {
+	private static final int MINITES_OF_UNIT = 30;
+
 	public Collection<Schedule> getFreeTime(LocalDate from, LocalDate to, Duration duraration) {
 		Collection<Schedule> temporaryFreeTime = createTemporaryFreeTime(from, to);
 		Collection<Schedule> schedules = getSchedules(from, to);
@@ -21,6 +24,16 @@ public class ScheduleManager {
 	}
 
 	private Collection<Schedule> getSchedules(LocalDate from, LocalDate to, String... names) {
+		Collection<Schedule> schedules = new HashSet<Schedule>();
+		for (ScheduleOwner scheduleOwner : getScheduleOwners()) {
+			for (Schedule schedule: scheduleOwner.getScheduleList()) {
+				schedules.add(schedule);
+			}
+		}
+		return schedules;
+	}
+
+	private Collection<ScheduleOwner> getScheduleOwners() {
 		throw new UnsupportedOperationException("未実装");
 	}
 
@@ -28,9 +41,9 @@ public class ScheduleManager {
 		Collection<Schedule> freeTimes = new LinkedList<>();
 		for (LocalDate date = from; date.isBefore(to) || date.equals(to); date = date.plusDays(1)) {
 			for (LocalTime time = RegularTime.START.getTime(); time
-				.isBefore(RegularTime.END.getTime()); time = time.plusMinutes(30)) {
+				.isBefore(RegularTime.END.getTime()); time = time.plusMinutes(MINITES_OF_UNIT)) {
 				LocalDateTime start = LocalDateTime.of(date, time);
-				LocalDateTime end = start.plusMinutes(30);
+				LocalDateTime end = start.plusMinutes(MINITES_OF_UNIT);
 				freeTimes.add(new Schedule(start, end));
 			}
 		}
